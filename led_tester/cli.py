0542fa3f8dae41b111919d01b0000b525ccba3d0
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-03-02 12:01:26 +0000

    added tests to check file reads correctly

--- led_tester/cli.py
+++ led_tester/cli.py
@@ -3,16 +3,21 @@
 """Console script for assignment3."""
 
 import click
-
+import sys
+click.disable_unicode_literals_warning=True
 
 @click.command()
 @click.option("--input", default=None, help="input URI(file or URL)")
 def main(input=None): 
 	"""Console script for assignment3."""
 	print ("input", input)
+	N, instructions = parseFile(input)
+	ledTester=LEDTester(N)
+	for instruction in instructions:
+		ledTester.apply(instruction)
+	print ('number occupied: ', ledTester.countOccupied())
 	return 0
 
 
 if __name__ == "__main__":
-    import sys
     sys.exit(main())
