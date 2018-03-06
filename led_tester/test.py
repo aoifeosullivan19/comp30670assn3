f2c34abbdcd0cbb9b863947c52a4fb9b31a0c40a
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-03-06 19:52:50 +0000

    created a grid from file, initialised all numbers to false, and trying to create some tests for this

--- led_tester/test.py
+++ led_tester/test.py
@@ -3,30 +3,47 @@ import urllib.request
 import requests
 import sys
 
-def parseFile(input):
-    if input.startswith('http'):
-        uri=input
-        req=urllib.request.urlopen(uri)
-        buff=req.read().decode('utf-8')
-        r = requests.get(uri).text
-    else:
-        filename = sys.argv[1]
-        with open (filename) as f:
-            for line in f.readlines():
-                values = [line.rstrip('\n') for line in open (filename)]
-                grid_num=values[0]
-            return int(grid_num) 
+class LED: 
+ 
+    def parseFile(input):
+        if input.startswith('http'):
+            uri=input
+            req=urllib.request.urlopen(uri)
+            buffer=req.read().decode('utf-8')
+            r = requests.get(uri).text
+            for line in r:
+                return ('\n'.join(r.split('\n')))
+        else:
+            filename = sys.argv[1]
+            with open (filename) as f:
+                for line in f.readlines():
+                    values = [line.rstrip('\n') for line in open (filename)]
+                return values
 
-def grid(number):
-    N=number
-    a2d=[ list(range(i*N, i*N + N)) for i in range(N) ]
-    a2d=[off for i in range(N)]
-    global off = 0
-    global on = 1
-    return a2d.count(on)
+    def grid_number(input):
+        if input.startswith('http'):
+            uri=input
+            req=urllib.request.urlopen(uri)
+            buffer=req.read().decode('utf-8')
+            r = requests.get(uri).text
+            for line in r:
+                return int(('\n'.join(r.split('\n')[:1])))
+        else:
+            filename = sys.argv[1]
+            with open (filename) as f:
+                for line in f.readlines():
+                    values = [line.rstrip('\n') for line in open (filename)]
+                    grid_num=values[0]
+                return int(grid_num)
+
+    def grid(number):
+        N=int(number)
+        a2d=[ list(range(i*N, i*N + N)) for i in range(N) ]
+        a2d=[False for i in range(N)]
+        return a2d
+
 
 input = sys.argv[1]
-grid_num=parseFile(input)
-grid(grid_num)
-print(grid_num)
-	
+number=LED.grid_number(input)
+print(LED.grid(number))
+print(sum(LED.grid(number)))	
---
+++ tests/test_grid_creation.py
@@ -0,0 +1,7 @@
+import unittest
+from led_tester import test
+
+def test_gridnumber(self):
+    g=LED.number(-3)
+    self.assertFalse(g)
+ 
