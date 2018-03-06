9f840f98e20eb54693ee8464b1750c93ebc611ee
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-03-06 23:33:04 +0000

    added to test file so i can search for strings and numbers from the file and implemented a 'turn on' function

--- led_tester/test.py
+++ led_tester/test.py
@@ -12,13 +12,39 @@ class LED:
             buffer=req.read().decode('utf-8')
             r = requests.get(uri).text
             for line in r:
-                return ('\n'.join(r.split('\n')))
+                lines=('\n'.join(r.split('\n')))
+                pat=re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+                if lines == pat:
+                    print("yay")
+                else:
+                    print ("no")
+            
         else:
             filename = sys.argv[1]
             with open (filename) as f:
                 for line in f.readlines():
                     values = [line.rstrip('\n') for line in open (filename)]
-                return values
+                    pat=re.match(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*", ".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+                numbers=[int(s) for s in re.findall(r'\b\d+\b', values[1])]
+                x1=numbers[0]
+                y1=numbers[1]
+                x2=numbers[2]
+                y2=numbers[3]
+                
+                str1='turn on'
+                str2='turn off'
+                str3='switch'
+                def turn_on(x1, y1, x2, y2):
+                    x_values = [True for i in range(x1, x2)]
+                    y_values = [True for i in range(y1, y2)]
+                    return sum(x_values +  y_values)
+                match1=re.search(r'turn on', values[1])
+                match1=re.findall(r'.*(turn on)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*', values[1])
+                match2=re.search(r'values[1]', str2)
+                match3=re.search(r'values[1]', str3)
+                if match1:
+                    return turn_on(x1, y1, x2, y2)
+                    
 
     def grid_number(input):
         if input.startswith('http'):
@@ -45,5 +71,6 @@ class LED:
 
 input = sys.argv[1]
 number=LED.grid_number(input)
-print(LED.grid(number))
-print(sum(LED.grid(number)))	
+a2d=LED.grid(number)
+print(LED.parseFile(input))
+
